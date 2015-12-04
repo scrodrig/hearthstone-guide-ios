58a939ad08d77968f9c1586926a3dd118524ee21
Schubert David Rodriguez <schubert_david@hotmail.com>
2015-12-04 18:46:19 +0000

    Initial Commit

---
+++ Hearthstone Beginners Guide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hearthstone Beginners Guide
+//
+//  Created by Schubert David Rodríguez on 04/12/15.
+//  Copyright © 2015 Schubert David Rodríguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
