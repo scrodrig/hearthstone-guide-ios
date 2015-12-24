f78de77d170b14aec3f4346ceccbf42fc6509a79
Schubert David Rodriguez <schubert_david@hotmail.com>
2015-12-24 19:18:58 +0000

    Menu for pattern, view

---
+++ Hearthstone Beginners Guide/ClassesPatternrecognitionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ClassesPatternrecognitionViewController.swift
+//  Hearthstone Beginners Guide
+//
+//  Created by Schubert David Rodríguez on 24/12/15.
+//  Copyright © 2015 Schubert David Rodríguez. All rights reserved.
+//
+
+import UIKit
+
+class ClassesPatternrecognitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
